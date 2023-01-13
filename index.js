d74191566520e727ef6faedbceb99ca0a4e64038
Bilada08 <joaofgamba@gmail.com>
2023-01-13 13:03:10 -0300

    access_control-change

--- index.js
+++ index.js
@@ -4,14 +4,26 @@ const authRoutes = require('./routes/auth.js')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 require('dotenv').config();
 
-app.use(cors({
-    origin: ['http://localhost:3000/', 'https://eice-chat.netlify.app/']
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+
+app.use(( req, res ) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        'https://eice-chat.netlify.app/',
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "GET, POST, PUT, DELETE, OPTIONS",
+    );
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World');
