3c28ff81bc244cb01ca02773332cb6c28bebc6cb
Bilada08 <joaofgamba@gmail.com>
2023-01-13 11:23:39 -0300

    health_check

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/auth.js');
 
+router.get('/health', async (req, res) =>  res.status(200).send("ok"))
 router.post('signup', signup);
 router.post('login', login);
 
